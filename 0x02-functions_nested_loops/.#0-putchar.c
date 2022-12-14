0096a4328cb361eb5da8e79afd82d692f260b0ed
Musa <117835082+CajunFoxy@users.noreply.github.com>
2022-12-14 23:09:39 +0200

    Update .#0-putchar.c
    fixed code

--- 0x02-functions_nested_loops/.#0-putchar.c
+++ 0x02-functions_nested_loops/.#0-putchar.c
@@ -1,1 +1,22 @@
-root@e955286010be.6778
+#include "main.h"
+
+/**
+* main -  prints _putchar, followed by a new line
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+char *sh = "Holberton";
+
+while (*sh)
+{
+putchar(*sh);
+sh++;
+}
+putchar('\n');
+
+return (0);
+}
